389e4c835bf071778839a4c827d71004206713e6
Guillermo Ortiz <guillermo.ortiz@compass.com>
2021-07-30 19:49:47 -0700

    Change subject strategy to record name and swtich from schema file to subject name

--- utils/parse_command_line_args.py
+++ utils/parse_command_line_args.py
@@ -7,7 +7,7 @@ def parse_command_line_args():
     arg_parser.add_argument("--topic", required=True, help="Topic name")
     arg_parser.add_argument("--bootstrap-servers", required=False, default="localhost:9092", help="Bootstrap server address")
     arg_parser.add_argument("--schema-registry", required=False, default="http://localhost:8081", help="Schema Registry url")
-    arg_parser.add_argument("--schema-file", required=False, help="File name of Avro schema to use")
+    arg_parser.add_argument("--schema-subject-name", required=False, help="The subject name of the Avro schema to be used")
     arg_parser.add_argument("--record-key", required=False, type=str, help="Record key. If not provided, will be a random UUID")
     arg_parser.add_argument("--record-value", required=False, help="Record value")
 
